fcf4ced31a454a96959c549960c2fffeefc3c019
Robby Fairclough <robby.fairclough@gmail.com>
2019-12-17 13:31:00 -0800

    update start

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-# stop script on error
+# Stop script on error
 set -e
 
 # Check to see if root CA file exists, download if not
@@ -7,12 +7,12 @@ if [ ! -f ./root-CA.crt ]; then
   curl https://www.amazontrust.com/repository/AmazonRootCA1.pem > root-CA.crt
 fi
 
-# install NPM packages
+# Install NPM packages
 if [ ! -d ./node_modules ]; then
   printf "\nInstalling NPM packages\n"
   npm install
 fi
 
-# run pub/sub sample app using certificates downloaded in package
+# Run
 printf "\nRunning Project Raptor\n"
 node index.js --host-name=a2x4jnyxq2i9zz-ats.iot.us-east-1.amazonaws.com --private-key=PI4.private.key --client-certificate=PI4.cert.pem --ca-certificate=root-CA.crt --client-id=sdk-nodejs-fbd45dbe-7fd7-4d3f-b89a-37b5ad8b04bb
